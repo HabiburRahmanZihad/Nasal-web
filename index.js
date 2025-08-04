18d78cdb16bb9ab35c8b6d991b23d08d577fe995
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-08-04 23:32:03 +0600

    Making Why Choose Our Nasal clip  section

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ const mobileMenu = document.getElementById("mobile-menu");
 menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
 });
+
+
+const carousel = document.getElementById('carousel');
+            const prevBtn = document.getElementById('prevBtn');
+            const nextBtn = document.getElementById('nextBtn');
+            let currentIndex = 0;
+
+            prevBtn.addEventListener('click', () => {
+                currentIndex = (currentIndex === 0) ? 3 : currentIndex - 1;
+                carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+            });
+
+            nextBtn.addEventListener('click', () => {
+                currentIndex = (currentIndex === 3) ? 0 : currentIndex + 1;
+                carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+            });
