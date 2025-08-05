3cc3fb0a2f3874e329691cb92641d34d1209a48c
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-08-05 18:35:23 +0600

    Make Preview  section

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ menuBtn.addEventListener("click", () => {
 
 
 const carousel = document.getElementById('carousel');
-            const prevBtn = document.getElementById('prevBtn');
-            const nextBtn = document.getElementById('nextBtn');
-            let currentIndex = 0;
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let currentIndex = 0;
 
-            prevBtn.addEventListener('click', () => {
-                currentIndex = (currentIndex === 0) ? 3 : currentIndex - 1;
-                carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-            });
+prevBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === 0) ? 3 : currentIndex - 1;
+    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
 
-            nextBtn.addEventListener('click', () => {
-                currentIndex = (currentIndex === 3) ? 0 : currentIndex + 1;
-                carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-            });
+nextBtn.addEventListener('click', () => {
+    currentIndex = (currentIndex === 3) ? 0 : currentIndex + 1;
+    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
